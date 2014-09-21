3bc28b654e3265cf05bae4bf40bfb1be11e88c03
Lexman <musclean@lexman.org>
2014-09-21 10:36:35 +0200

    Un peu moins moche + un décompte

--- trampo_timer.py
+++ trampo_timer.py
@@ -1,14 +1,21 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
 
 class TimerContainer(BoxLayout):
-    pass
+    count_down = 100
+    def tick(self, dt):
+        self.decompte.text = str(self.count_down)
+        self.count_down = self.count_down - 1
 
+    #def saisie_nb_passages(
 
 class TimerApp(App):
     def build(self):
-        return TimerContainer()
+        container = TimerContainer()
+        Clock.schedule_interval(container.tick, 1.0)
+        return container
 
 
 if __name__ == '__main__':
